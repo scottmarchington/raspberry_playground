c5298cdcf3caac406441b8b329e1133922c0d407
Scott Marchington <scott.marchington@gmail.com>
2017-12-03 16:10:32 -0800

    Add MotorSpinner

---
+++ Sources/led-blink/MotorSpinner.swift
@@ -0,0 +1,17 @@
+//
+//  MotorSpinner.swift
+//  led-blinkPackageDescription
+//
+//  Created by Scott Marchington on 12/3/17.
+//
+
+import Foundation
+
+class MotorSpinner: Routine {
+    let commandLineName: String = "motor"
+    let humanReadableName: String = "Motor Spinner"
+    
+    func start(on gpio: GPIOName) {
+        print("This routine has not been implemented yet")
+    }
+}
