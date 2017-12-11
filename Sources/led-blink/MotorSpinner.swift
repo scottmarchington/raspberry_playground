0b52c8beffd2b21d1a50fb3cb318c7e19cdd7598
Scott Marchington <scott.marchington@gmail.com>
2017-12-11 01:11:48 -0800

    Added MotorSpinnerWorker and debug code

--- Sources/led-blink/MotorSpinner.swift
+++ Sources/led-blink/MotorSpinner.swift
@@ -20,5 +20,47 @@ class MotorSpinner: Routine {
     
     func start(on gpio: GPIOName) {
         print("This routine has not been implemented yet")
+        if let worker = MotorSpinnerWorker() {
+            print("worker successfully instantiated")
+            // Worker successfully instantiated
+        } else {
+            print("worker failed to be instantiated")
+            // Worker failed to be instantiated
+        }
+    }
+}
+
+class MotorSpinnerWorker {
+    let interface: I2CInterface
+    
+    init?() {
+        guard let i2cs = SwiftyGPIO.hardwareI2Cs(for: .RaspberryPi3) else {
+            print("Couldn't find I2C interfaces")
+            return nil
+            
+        }
+        
+        guard i2cs.count > 1 else {
+            print("Not enough interfaces (has \(i2cs.count))")
+            return nil
+        }
+        
+        interface = i2cs[1]
+        
+        if interface.isReachable(0x60) {
+            print("0x60 is reachable!")
+        } else {
+            print("0x60 is not reachable")
+        }
+        
+        if interface.isReachable(0x70) {
+            print("0x70 is reachable!")
+        } else {
+            print("0x70 is not reachable")
+        }
+    }
+    
+    func set(to val: Float) {
+        
     }
 }
