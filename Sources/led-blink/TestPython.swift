35c117ff0e5a6154be1fc75a782d9613211e47ad
Scott Marchington <scott.marchington@gmail.com>
2017-12-16 14:58:15 -0800

    Try different path

--- Sources/led-blink/TestPython.swift
+++ Sources/led-blink/TestPython.swift
@@ -11,6 +11,8 @@ import PerfectPython
 
 class TestPython {
     init() throws {
-        let pymodule = try PyObj(path: "/Adafruit_MotorHAT/Adafruit_MotorHAT_Motors", import: "Adafruit_MotorHAT")
+        let pymodule = try PyObj(path: "../../Sources/Adafruit_MotorHAT/Adafruit_MotorHAT_Motors", import: "Adafruit_MotorHAT")
     }
 }
+
+
