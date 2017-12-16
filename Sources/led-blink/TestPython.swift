8a22bdce434a96af235ee60c788bd59e82f2754e
Scott Marchington <scott.marchington@gmail.com>
2017-12-16 15:15:40 -0800

    Keep on trying

--- Sources/led-blink/TestPython.swift
+++ Sources/led-blink/TestPython.swift
@@ -11,7 +11,9 @@ import PerfectPython
 
 class TestPython {
     init() throws {
-        _ = try PyObj(path: "./../../Sources/led-blink/Adafruit_MotorHAT", import: "Adafruit_MotorHAT_Motors")
+        let driverModule = try PyObj(path: "./../../Sources/led-blink/Adafruit_MotorHAT", import: "Adafruit_PWM_Servo_Driver")
+        
+        let motorModule = try PyObj(path: "./../../Sources/led-blink/Adafruit_MotorHAT", import: "Adafruit_MotorHAT_Motors")
     }
 }
 
