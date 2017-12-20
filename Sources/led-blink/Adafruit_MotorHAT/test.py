41632e2c45a160471eeea3c9107fb6bc7e1c51a9
scottmarchington <scott.marchington@gmail.com>
2017-12-20 10:58:14 -0800

    Delete test.py

--- Sources/led-blink/Adafruit_MotorHAT/test.py
+++
@@ -1,7 +0,0 @@
-from Adafruit_MotorHAT.Adafruit_MotorHAT_Motors import Adafruit_StepperMotor, Adafruit_DCMotor, Adafruit_MotorHAT
-
-class HATFactory:
-    def makeHAT():
-        return Adafruit_MotorHAT()
-
-
